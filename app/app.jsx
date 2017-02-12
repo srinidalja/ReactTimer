79841123e1f66909af394474a95e0fd178035362
Srinivas D <srinivas.dalja@icentris.com>
2017-02-12 14:29:40 +0530

    Add navigation

--- app/app.jsx
+++ app/app.jsx
@@ -1,6 +1,8 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var Main = require('Main');
+var Timer = require('Timer');
+var Countdown = require('Countdown');
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 
 // Load Foundation
@@ -13,6 +15,8 @@ require('style!css!sass!applicationStyles')
 ReactDOM.render(
   <Router history={hashHistory}>
     <Route path="/" component={Main}>
+      <Route path="countdown" component={Countdown} />
+      <IndexRoute component={Timer} />
     </Route>
   </Router>,
   document.getElementById('app')
--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -1,5 +1,6 @@
 var React = require('react');
 var {Link, IndexLink} = require('react-router');
+
 var Nav = React.createClass ({
     render: function(){
         return(
@@ -7,8 +8,12 @@ var Nav = React.createClass ({
                 <div className="top-bar-left">
                     <ul className="menu">
                         <li className="menu-text">React Timer App</li>
-                        <li><IndexLink to="/" activeClassName="active" activeStyle={{fontWeight:'bold'}}>Timer</IndexLink></li>
-                        <li><Link to="/countdown" activeClassName="active" activeStyle={{fontWeight:'bold'}}>Countdown</Link></li>
+                        <li>
+                            <IndexLink to="/" activeClassName="active-link">Timer</IndexLink>
+                        </li>
+                        <li>
+                            <Link to="/countdown" activeClassName="active-link">Countdown</Link>
+                        </li>
                     </ul>
                 </div>
                 <div className="top-bar-right">
